dc7326fc79bdcc6c45fed2258c936e0f8cf45ec7
walid.devops <walid.devops@gmail.com>
2026-04-22 14:11:24 +0200

    Refactor MainMenu and config.prod.mjs for improved readability and consistency

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -1,34 +1,39 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
-    }
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+  create() {
+    this.add.image(512, 384, "background");
 
-        this.add.image(512, 300, 'logo');
+    this.add.image(512, 300, "logo");
 
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.add
+      .text(512, 460, "Main Menu", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.add.text(512, 550, 'Walid BEN AYYAD DEVOXX 2026', {
-            fontFamily: 'Arial Black', fontSize: 24, color: '#ffcc00',
-            stroke: '#000000', strokeThickness: 4,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.add
+      .text(512, 550, "Walid BEN AYYAD DEVOXX 2026", {
+        fontFamily: "Arial Black",
+        fontSize: 24,
+        color: "#ffcc00",
+        stroke: "#000000",
+        strokeThickness: 4,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
-        });
-    }
+    this.input.once("pointerdown", () => {
+      this.scene.start("Game");
+    });
+  }
 }
